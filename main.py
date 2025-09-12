4998c958a4f8f458a8f5c95273caa62451b2ac28
心海海 <a3049807945@163.com>
2025-09-12 18:33:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -61,7 +61,8 @@ class AkashaTerminal(Star):
 
             message = (
                 f"用户信息:\n"
-                f"姓名: {user_id}\n"
+                f"姓名:{user_name}\n"
+                f"ID: {user_id}\n"
                 f"等级: {user_data.get('level', 1)}\n"
                 f"经验: {battle_data.get('experience', 0)}\n"
                 f"金钱: {home_data.get('money', 0)}\n"
