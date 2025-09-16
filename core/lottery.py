0d65eca164c0985e078d81194f7d0c5a780c053f
心海海 <a3049807945@163.com>
2025-09-16 19:05:55 +0800

    Update lottery.py

--- core/lottery.py
+++ core/lottery.py
@@ -13,7 +13,7 @@ from ..utils.utils import read_json, write_json
 
 
 class lottery:
-    def __init__(self):
+    def __init__(self, items_with_weight):
         # 初始化抽奖配置
         self.weapon_data = None  # 初始化属性
 
@@ -28,27 +28,27 @@ class lottery:
         except Exception as e:
             logger.error(f"加载抽奖配置时发生未知错误: {e}")
 
-    async def draw(self, times: int):
-        xhh = int(1)  # 默认抽奖次数
-        weapon = []  # 存储抽取武器名称
+        def lottery(self):
+            """执行一次抽奖"""
+            self.items = ["五星", "四星", "三星"]  # 获奖
+            self.weights = ["1", "2", "17"]  # 权重
+            rand_sum = random.uniform(0, 20)  # 生成一个随机数,在0~20之间
 
-        for xhh in range(0, times):
-            q = random.randint(1, 313)
-            # 五星
-            if q >= 1 and q <= 32:
-                a = random.randint(44, 75)  # 抽取武器序号
-                weapon_id = str(a)
-                b = self.weapon_data[weapon_id]["name"]  # 武器名称
-                weapon.append(b)
-            # 四星
-            elif q >= 33 and q <= 93:
-                a = random.randint(1, 30)
-                weapon_id = str(a)
-                b = self.weapon_data[weapon_id]["name"]
-                weapon.append(b)
-            # 三星
-            elif q >= 94 and q <= 313:
-                a = random.randint(31, 43)
-                weapon_id = str(a)
-                b = self.weapon_data[weapon_id]["name"]
-                weapon.append(b)
+            current = 0
+
+            for i in range(0, 3):
+                current += self.weights[i]
+                if rand_sum <= current:
+                    main_prizes = self.items
+
+                    if main_prizes == "五星":
+                        a = random.randint(500, 531)
+                        print(f"{self.weapon_data[a]['name']}")
+
+                    elif main_prizes == "四星":
+                        a = random.randint(400, 429)
+                        print(f"{self.weapon_data[a]['name']}")
+
+                    else:
+                        a = random.randint(300, 312)
+                        print(f"{self.weapon_data[a]['name']}")
